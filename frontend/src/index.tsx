cb4986928f77e07c60413dbe0cfb50143b2f382b
Jakub Cerovsky <jacobcerovsky@gmail.com>
2024-04-14 15:35:07 +0200

    Clean-Up React App

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { AddStory, Stories, Story, Department, Departments } from "./pages";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Stories />,
+      },
+      {
+        path: "/AddStory",
+        element: <AddStory />,
+      },
+      {
+        path: "/:storyId",
+        element: <Story />,
+      },
+    ],
+  },
+  {
+    path: "/Deparments",
+    element: <Departments />,
+    children: [
+      {
+        path: "Departments/:departmentId",
+        element: <Department />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
