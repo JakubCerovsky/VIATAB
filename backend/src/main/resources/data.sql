bbf8c064f4043888c6a10fd6a6a48c3c9867e3e5
Jakub Cerovsky <jacobcerovsky@gmail.com>
2024-04-15 15:06:14 +0200

    Refactoring backend configurations

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO departments (name) VALUES ('Architecture');
 INSERT INTO departments (name) VALUES ('Marketing');
 
 INSERT INTO stories (title, description, department_id) VALUES ('ChatGPT', 'Is it worth paying for GPT4?', 1);
+INSERT INTO stories (title, description, department_id) VALUES ('Arduino', 'Cheap, yet effective!', 2);
+INSERT INTO stories (title, description, department_id) VALUES ('Instagram', 'Better version of other SoMe!', 4);
 INSERT INTO stories (title, description, department_id) VALUES ('Gemini', 'New, Free, and Better GPT!', 1);
-INSERT INTO stories (title, description, department_id) VALUES ('Arduino', 'Cheap, yet effective!', 3);
-INSERT INTO stories (title, description, department_id) VALUES ('Instagram', 'Better version of other SoMe!', 2);
-INSERT INTO stories (title, description, department_id) VALUES ('Concrete', 'Best building substance!', 4);
-INSERT INTO stories (title, description, department_id) VALUES ('Facebook', 'Are you still using it?', 2);
+INSERT INTO stories (title, description, department_id) VALUES ('Concrete', 'Best building substance!', 3);
+INSERT INTO stories (title, description, department_id) VALUES ('Facebook', 'Are you still using it?', 4);
